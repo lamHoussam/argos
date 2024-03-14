189cec14c1c97bb529be943a774f25c3e1b777b8
houssam lamlih <houssam.lamlih@hotmail.com>
2024-03-14 14:07:24 -0400

    Find function calls; Get type of variables; Parse strcpy arguments to detect simple buffer overflow

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, f32::consts::E};
+use std::collections::HashMap;
 use clang::{Entity, EntityKind};
 
 #[derive(Debug)]
 pub struct Variable {
     pub name: String,
     pub size: usize,
+    pub var_type: String,
 }
 
 #[derive(Debug)]
@@ -13,10 +14,11 @@ pub struct CodeParser {
 }
 
 impl Variable {
-    pub fn new(var_name: String, var_size: usize) -> Self {
+    pub fn new(var_name: String, var_size: usize, var_type: String) -> Self {
         Variable {
             name: var_name,
             size: var_size,
+            var_type: var_type,
         }
     }
 }
@@ -28,8 +30,18 @@ impl CodeParser {
         }
     }
 
-    pub fn add_new_variable(&mut self, var_name: String, size: usize) {
-        self.variables.insert(var_name.clone(), Variable::new(var_name, size));        
+    pub fn add_new_variable(&mut self, var_name: String, size: usize, var_type: String) {
+        self.variables.insert(var_name.clone(), Variable::new(var_name, size, var_type));
+    }
+
+    fn parse_strcpy(&self, args: &Vec<Entity<'_>>) -> bool {
+        let dest = args[0];
+        let srce = args[1];
+
+        let var_dest = self.variables.get(&dest.get_display_name().unwrap()).expect("Variable not declared");
+        let var_srce = self.variables.get(&srce.get_display_name().unwrap()).expect("Variable not declared");
+
+        var_srce.size >= var_dest.size
     }
 
     pub fn parse_code(&mut self, entity: &Entity<'_>) {
@@ -37,51 +49,70 @@ impl CodeParser {
         // println!("Name: {:?}; Kind: {:?}", entity.get_name(), entity.get_kind());  
         // println!("Ent: {:?}", entity);
 
-        if entity.get_kind() == EntityKind::VarDecl {
-            let tpe = entity.get_type().unwrap();
-            match tpe.get_kind() {
-                clang::TypeKind::ConstantArray => {
-                    // TODO: Cleaup
-                    let display_name = tpe.get_display_name();
-                    let display_name_split: Vec<&str> = display_name.split('[').collect();
-                    let type_name = display_name_split.first().unwrap();
-                    let size_str = display_name_split.last().unwrap().trim_end_matches(']');
-                    let size = size_str.parse::<usize>().unwrap_or_default();
+        match entity.get_kind() {
+            EntityKind::VarDecl => {
+                let tpe = entity.get_type().unwrap();
+                match tpe.get_kind() {
+                    clang::TypeKind::ConstantArray => {
+                        // TODO: Cleaup
+                        let display_name = tpe.get_display_name();
+                        let display_name_split: Vec<&str> = display_name.split('[').collect();
+                        let type_name = display_name_split.first().unwrap();
+                        let size_str = display_name_split.last().unwrap().trim_end_matches(']');
+                        let size = size_str.parse::<usize>().unwrap_or_default();
 
-                    // TODO: Multiply size by sizeof(type_name)
-                    println!("name: {:?}, size: {:?}", type_name, size);
-                    self.add_new_variable(entity.get_name()
-                        .unwrap_or("Variable".to_string()), size);
-                },
-                clang::TypeKind::Pointer => {
-                    // Only for string literal initialisation or empty declaration
-                    let display_name = tpe.get_display_name();
-                    if let Some(child) = entity.get_child(0) {
-                        let value = child.get_child(0).unwrap().get_display_name().unwrap_or_default();
-                        let size = value.len() - 1; // Remove "" and add \0 in count
-                        println!("Found value: {}, size: {}", value, size);
+                        // TODO: Multiply size by sizeof(type_name)
+                        println!("name: {:?}, size: {:?}, type: {:?}", type_name, size, type_name);
                         self.add_new_variable(entity.get_name()
-                            .unwrap_or("Variable".to_string()), size)
-                    } else {
-                        self.add_new_variable(entity.get_name()
-                            .unwrap_or("Variable".to_string()), 0)
+                            .unwrap_or("Variable".to_string()), size, type_name.to_string());
+                    },
+                    clang::TypeKind::Pointer => {
+                        // Only for string literal initialisation or empty declaration
+                        let type_name = tpe.get_display_name();
+                        if let Some(child) = entity.get_child(0) {
+                            let value = child.get_child(0).unwrap().get_display_name().unwrap_or_default();
+                            let size = value.len() - 1; // Remove "" and add \0 in count
+                            println!("Found value: {}, size: {}, displ name: {}", value, size, type_name);
+                            self.add_new_variable(entity.get_name()
+                                .unwrap_or("Variable".to_string()), size, type_name)
+                        } else {
+                            self.add_new_variable(entity.get_name()
+                                .unwrap_or("Variable".to_string()), 0, type_name)
+                        }
+
+                        // TODO: Multiply size by sizeof(type_name)
+                        // println!("name: {:?}", display_name);
+                    }, 
+                    _ => {
+                        
                     }
-
-                    // TODO: Multiply size by sizeof(type_name)
-                    println!("name: {:?}", display_name);
                 }
-                _ => {
 
+                // println!("Entity: {:?}", tpe);
+                // println!("Type: {:?}, other: {:?}", tpe, tpe.get_kind());
+                // println!("=> Var Entity: {:?}, Type: {:?}", entity, entity.get_type());
+
+                // println!("-> Children: {:?}", entity.get_children());
+
+            }, 
+            EntityKind::CallExpr => {
+                // let tpe = entity.get_type().unwrap();
+                let display_name = entity.get_display_name().unwrap();
+                if display_name == "strcpy" {
+                    if let Some(args) = entity.get_arguments() {
+                        let found_buff_overflow = self.parse_strcpy(&args);
+                        if found_buff_overflow {
+                            println!("Detected buffer overflow at line {:?}", entity.get_location().unwrap().get_file_location());
+                        }
+                    }
                 }
+                // println!("Function call: {}", display_name);
+
+            }, 
+            _ => {
+                
             }
-
-            println!("Entity: {:?}", tpe);
-            // println!("Type: {:?}, other: {:?}", tpe, tpe.get_kind());
-            // println!("=> Var Entity: {:?}, Type: {:?}", entity, entity.get_type());
-
-            // println!("-> Children: {:?}", entity.get_children());
         }
-        
         let children = &entity.get_children();
         if children.len() == 0 { return; }
 
